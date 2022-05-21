5a02f4d8419bc2bde85dd2b39d9de2ec45658044
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-21 01:38:48 -0300

    clase5-correcciones

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // CLASE 5 - Lunes 2 de mayo de 2022
-// Estructuras de Control 
+// Estructuras de Control
 
 // Ejercicio 9: Calificaciones
 
@@ -16,17 +16,23 @@ no imprimirá las notas, mostrará un mensaje de error
 y continuará con otro alumno) */
 
 let nombreAlumno = prompt("Ingrese el nombre del alumno (vacío para cortar)");
-let notaPractica, notaProblemas, notaTeorica, notaFinal : number;
+let notaPractica, notaProblemas, notaTeorica, notaFinal: number;
 while (nombreAlumno !== "") {
-  notaPractica = Number(prompt("Nota práctica: " ));
-  notaProblemas = Number(prompt("Nota problemas: " ));
-  notaTeorica = Number(prompt("Nota teórica: " ));
-  if ((notaPractica <= 10 && notaPractica >= 0) && (notaProblemas <= 10 && notaProblemas >= 0) && 
-    (notaTeorica <= 10 && notaTeorica >= 0) {
-    notaFinal = notaPractica * 0.1 + notaProblemas * 0.5 + notaTeorica * 0.4 ;
-    console.log ("La nota final de ", nombreAlumno, " es: ", notaFinal );
+  notaPractica = Number(prompt("Nota práctica: "));
+  notaProblemas = Number(prompt("Nota problemas: "));
+  notaTeorica = Number(prompt("Nota teórica: "));
+  if (
+    notaPractica <= 10 &&
+    notaPractica >= 0 &&
+    notaProblemas <= 10 &&
+    notaProblemas >= 0 &&
+    notaTeorica <= 10 &&
+    notaTeorica >= 0
+  ) {
+    notaFinal = notaPractica * 0.1 + notaProblemas * 0.5 + notaTeorica * 0.4;
+    console.log("La nota final de ", nombreAlumno, " es: ", notaFinal);
   } else {
-    console.log ("Error en las notas ingresadas ");
+    console.log("Error en las notas ingresadas ");
   }
-  nombreAlumno = prompt("Ingrese el nombre del alumno (vacío para cortar) ");
+  nombreAlumno = prompt("Ingrese el nombre del alumno (vacío para cortar)");
 }
